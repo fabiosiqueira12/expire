7f06c8e09a74e3102845a935aa02cf3423e8cfe1
fabiosiqueira12 <fabio.siqueira1222@gmail.com>
2018-07-18 22:39:32 -0300

    Altera meta tags

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
-    
+    $(".desce-efeito").click(function (event) {
+        event.preventDefault();
+        $('body,html').animate({
+            scrollTop: $($(this).attr("href")).offset().top - 78
+        }, 700);
+    });
 });
