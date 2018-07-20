27fab64b62fa89d275e271a9f8fa25926d9af51c
fabiosiqueira12 <fabio.siqueira1222@gmail.com>
2018-07-19 21:55:18 -0300

    Adiciona novas sessões

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,22 @@
 $(document).ready(function () {
+    
+    if ($(window).scrollTop() > $("#image-banner").outerHeight()) {
+        $(".header-scroll").slideDown();
+    } else {
+        $(".header-scroll").slideUp();
+    }
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > $("#image-banner").outerHeight()) {
+            $(".header-scroll").slideDown();
+        } else {
+            $(".header-scroll").slideUp();
+        }
+    });
+
     $(".desce-efeito").click(function (event) {
         event.preventDefault();
         $('body,html').animate({
-            scrollTop: $($(this).attr("href")).offset().top - 78
+            scrollTop: $($(this).attr("href")).offset().top + 20
         }, 700);
     });
 });
