12ea7bcaa5377b1159b7e3f8bd56f3043cd2740a
fabiosiqueira12 <fabio.siqueira1222@gmail.com>
2018-07-26 00:04:53 -0300

    Adiciona página com formulário

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,4 +19,11 @@ $(document).ready(function () {
             scrollTop: $($(this).attr("href")).offset().top + 20
         }, 700);
     });
+
+    $('.mask-cep').mask('00000-000');
+	$('.mask-phone').mask('(00) 00000-0000');
+	$('.mask-cnpj').mask('00.000.000/0000-00', {reverse: true});
+	$('.mask-cpf').mask('000.000.000-00', {reverse: true});
+	$('.mask-money').mask('#.##0,00', {reverse: true});
+    
 });
